ddb6c3b9847ac375d7a068b7159ce36ef6af589c
spencer kelly <spencerkelly86@gmail.com>
2022-08-26 17:22:53 -0400

    more issues

--- scratch.js
+++ scratch.js
@@ -21,6 +21,51 @@ txt = 'ventuno candeline'
   * ma è pericoloso fissarlo
 
 *  in particolare erano allarmanti e sono stati {allarmante} //adj
+
+dichiarazioni più lucide mai girate {lucido}
+
+una pensierosa Caterina  {pensieroso} //adj
+*  questi pezzi musicali pensosi
+
+risiede nella natura ripetitiva {ripetitivo} //adj
+
+in avanti rendendo retroattivi {retroattivo} //adj
+* la concessione di prestazioni retroattive fino
+
+indossare oggetti lucidi {lucido}  //adj
+
+Un paio di versi successivi {successivo} //adj
+* eventi degli anni successivi
+* le generazioni successive
+
+mescolati con solenni interviste {solenne} //adj
+
+La fastidiosa necessità {fastidioso}  //adj
+* delle persone fastidiose
+
+per i veicoli {veicolo} //noun
+
+Sono entrambe ottime risorse {risorsa} //noun
+
+
+ impegnate nei principi democratici {principio} //noun
+
+
+ inclini a fare cose ridicole {ridicolo} //adj
+
+ Mantenere buoni rapporti {rapporto} //noun
+
+ indossi quell'uniforme {uniforme} //noun
+
+ che ci avrebbero reso prosperi  {prospero} //adj
+
+ avere conseguenze sorprendenti {sorprendente} //adj
+
+  anche se dubiti delle prove {dubitare} //vb
+  * 	Ma non c'è dubbio che
+
+omaggi dei contribuenti {contribuente} //noun
+
 */
 
 txt = ' diecimila'

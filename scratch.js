b49a7c1fba517ab01e4fb8f9efbf1b8c6992c4a7
spencer kelly <spencerkelly86@gmail.com>
2022-08-30 10:23:53 -0400

    more root tests

--- scratch.js
+++ scratch.js
@@ -11,61 +11,10 @@ txt = 'Nel pomeriggio vado da Marco'//In the afternoon I’ll go to Marco’s pl
 txt = 'ventuno candeline'
 
 /*
-
-
-* Una retirada repentina {repentino} //adj
-* di stato furono assassinati  {assassinare} //verb
-  *  è stato assassinato
-  * 
-* dovresti fissarla {fissare} //verb
-  * ma è pericoloso fissarlo
-
-*  in particolare erano allarmanti e sono stati {allarmante} //adj
-
-dichiarazioni più lucide mai girate {lucido}
-
-una pensierosa Caterina  {pensieroso} //adj
-*  questi pezzi musicali pensosi
-
-risiede nella natura ripetitiva {ripetitivo} //adj
-
-in avanti rendendo retroattivi {retroattivo} //adj
-* la concessione di prestazioni retroattive fino
-
-indossare oggetti lucidi {lucido}  //adj
-
-Un paio di versi successivi {successivo} //adj
-* eventi degli anni successivi
-* le generazioni successive
-
-mescolati con solenni interviste {solenne} //adj
-
-La fastidiosa necessità {fastidioso}  //adj
-* delle persone fastidiose
-
-per i veicoli {veicolo} //noun
-
-Sono entrambe ottime risorse {risorsa} //noun
-
-
- impegnate nei principi democratici {principio} //noun
-
-
- inclini a fare cose ridicole {ridicolo} //adj
-
- Mantenere buoni rapporti {rapporto} //noun
-
- indossi quell'uniforme {uniforme} //noun
-
- che ci avrebbero reso prosperi  {prospero} //adj
-
- avere conseguenze sorprendenti {sorprendente} //adj
-
-  anche se dubiti delle prove {dubitare} //vb
-  * 	Ma non c'è dubbio che
-
-omaggi dei contribuenti {contribuente} //noun
-
+advice-noun-01
+amazing-adjective-01
+amazing-adjective-01
+clock-noun-01
 */
 
 txt = ' diecimila'
--- tests/root.test.js
+++ tests/root.test.js
@@ -63,8 +63,54 @@ test('root-match:', function (t) {
     ["e altre malattie mortali.", "{mortale}"], //adj
     ["è stato demolito", "{demolire}"], //verb
     ["sono gravemente carenti", "{carente}"], //adjective
-
     ['sfruttato dalla massiccia effusione', '{massiccio}'], // adj
+
+
+
+    ['Una retirada repentina', '{repentino}'], //adj
+    ['di stato furono assassinati ', '{assassinare}'], //verb
+    [' è stato assassinato', '{assassinare}'],
+    ['dovresti fissarla', '{fissare}'], //verb
+    ['ma è pericoloso fissarlo', '{fissare}'],
+    [' in particolare erano allarmanti e sono stati', '{allarmante}'], //adj
+    ['dichiarazioni più lucide mai girate', '{lucido}'],
+    ['una pensierosa Caterina ', '{pensieroso}'], //adj
+    [' questi pezzi musicali pensosi  ', '{pensieroso}'],
+    ['risiede nella natura ripetitiva', '{ripetitivo}'], //adj
+    ['in avanti rendendo retroattivi', '{retroattivo}'], //adj
+    ['la concessione di prestazioni retroattive fino', '{retroattivo}'],
+    ['indossare oggetti lucidi', '{lucido}'],  //adj
+    ['Un paio di versi successivi', '{successivo}'], //adj
+    ['eventi degli anni successivi', '{successivo}'],
+    ['le generazioni successive', '{successivo}'],
+    ['mescolati con solenni interviste', '{solenne}'], //adj
+    ['La fastidiosa necessità', '{fastidioso}'],  //adj
+    ['delle persone fastidiose', '{fastidioso}'],
+    ['per i veicoli', '{veicolo}'], //noun
+    ['Sono entrambe ottime risorse', '{risorsa}'], //noun
+    ['impegnate nei principi democratici', '{principio}'], //noun
+    ['inclini a fare cose ridicole', '{ridicolo}'], //adj
+    ['Mantenere buoni rapporti', '{rapporto}'], //noun
+    ['indossi quell\'uniforme', '{uniforme}'], //noun
+    ['che ci avrebbero reso prosperi ', '{prospero}'], //adj
+    ['avere conseguenze sorprendenti', '{sorprendente}'], //adj
+    ['anche se dubiti delle prove', '{dubitare}'], //vb
+    ['Ma non c\'è dubbio che', '{dubitare}'],
+    ['omaggi dei contribuenti', '{contribuente}'], //noun
+    ['Hai bisogno di consigli!', '{consiglio}'],//
+    [' e le braccia intorno al collo', '{braccio}'],
+    ['la ragazza più bella del mondo', '{bello}'],
+    [' le tue ossa e ritira', '{osso}'],
+    [' con i vostri capi.', '{capo}'],
+    ['Abbiamo strade e ponti fatiscenti.', '{ponte}'],
+    [' Andando come torte calde', '{torta}'],
+    ['gettato piccoli pezzi di ghiaccio', '{pezzo}'],
+    ['gli orologi suonavano', '{orologio}'],
+    ['Come nuvole sotto', '{nuvola}'],
+    ['Tra i mammiferi colorati ', '{colorato}'],
+    ['branco di astuti runts', '{astuto}'],
+    ['moglie era sorda', '{sordo}'],
+
   ]
   arr.forEach(function (a) {
     let [str, match] = a
